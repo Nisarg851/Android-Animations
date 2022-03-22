c684bf849affaab879e1981b275093fb9090d232
Nisarg851 <nisargmahyavanshi8501@gmail.com>
2022-03-22 16:30:52 +0530

    Fragments, Transactions and designs ready

---
+++ SmoothSlidingFragmentTransaction/app/src/main/java/com/example/smoothslidingfragmenttransaction/fragments/Fragment2.java
@@ -0,0 +1,33 @@
+package com.example.smoothslidingfragmenttransaction.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.smoothslidingfragmenttransaction.R;
+
+public class Fragment2 extends Fragment {
+
+    Button btnMove;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+        btnMove = view.findViewById(R.id.btnMove);
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        btnMove.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainerView, Fragment3.class, null);
+            fragmentTransaction.commit();
+        });
+
+        return view;
+    }
+}
---
+++ SmoothSlidingFragmentTransaction/app/src/main/java/com/example/smoothslidingfragmenttransaction/fragments/Fragment3.java
@@ -0,0 +1,34 @@
+package com.example.smoothslidingfragmenttransaction.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.smoothslidingfragmenttransaction.R;
+
+public class Fragment3 extends Fragment {
+
+    Button btnMove;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_3, container, false);
+
+        btnMove = view.findViewById(R.id.btnMove);
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        btnMove.setOnClickListener(v -> {
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainerView, Fragment1.class, null);
+            fragmentTransaction.commit();
+        });
+
+        return view;
+    }
+}
