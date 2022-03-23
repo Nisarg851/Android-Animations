208a3aa0a83d11b612ccf9bbb61dbb0c74991c43
Nisarg Mahyavanshi <nisargmahyavanshi8501@gmail.com>
2022-03-23 12:00:35 +0530

    Calander View Ready

--- SmoothSlidingFragmentTransaction/app/src/main/java/com/example/smoothslidingfragmenttransaction/fragments/Fragment2.java
+++ SmoothSlidingFragmentTransaction/app/src/main/java/com/example/smoothslidingfragmenttransaction/fragments/Fragment2.java
@@ -10,18 +10,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.smoothslidingfragmenttransaction.R;
 
 public class Fragment2 extends Fragment {
 
+    TextView tvText;
     Button btnMove;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_2, container, false);
+        tvText = view.findViewById(R.id.tvText);
         btnMove = view.findViewById(R.id.btnMove);
+
         FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+
+        String[] DateTimeMonthBundle = this.getArguments().getStringArray("DateTimeMonthBundle");
+        if(DateTimeMonthBundle!=null){
+            String recievedDate = DateTimeMonthBundle[0]+"/"+DateTimeMonthBundle[1]+"/"+DateTimeMonthBundle[2];
+            tvText.setText(recievedDate);
+        }
         btnMove.setOnClickListener(v -> {
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.replace(R.id.fragmentContainerView, Fragment3.class, null);
---
+++ SmoothSlidingFragmentTransaction/app/src/main/java/com/example/smoothslidingfragmenttransaction/recyclerAdapters/CalanderAdapter.java
@@ -0,0 +1,68 @@
+package com.example.smoothslidingfragmenttransaction.recyclerAdapters;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.smoothslidingfragmenttransaction.R;
+
+import java.util.ArrayList;
+
+public class CalanderAdapter extends RecyclerView.Adapter<CalanderAdapter.ViewHolder> {
+
+    private final ArrayList<String> datesOfCalander;
+    private final onItemListener listener;
+    private String[] DayMonthYear;
+
+    public CalanderAdapter(ArrayList<String> datesOfCalander, onItemListener listener) {
+        this.datesOfCalander = datesOfCalander;
+        this.listener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_calander_layout, parent, false);
+//        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+//        layoutParams.height = (int)(parent.getHeight()*0.166666666);
+        return new ViewHolder(view, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        DayMonthYear = datesOfCalander.get(position).split("/");
+        Log.e("bundle", "onBindViewHolder: "+datesOfCalander.get(position));
+        holder.tvCalanderDate.setText(DayMonthYear[0]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return datesOfCalander.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvCalanderDate;
+        CalanderAdapter.onItemListener listener;
+
+        public ViewHolder(View itemView, CalanderAdapter.onItemListener listener) {
+            super(itemView);
+            tvCalanderDate = itemView.findViewById(R.id.tvCalanderDate);
+            this.listener = listener;
+            tvCalanderDate.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            DayMonthYear = datesOfCalander.get(position).split("/");
+            listener.onItemClick(position, DayMonthYear);
+        }
+    }
+
+    public interface onItemListener{
+        void onItemClick(int position, String[] DayMonthYear);
+    }
+}
